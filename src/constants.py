5371197ede76d7c5dd454d33dac4a247aa586cac
resirch <sirchhistory@pm.me>
2025-08-03 13:36:52 -0500

    ytpo

--- src/constants.py
+++ src/constants.py
@@ -181,6 +181,6 @@ DEFAULT_CONFIG = {
             "peak_rank_act": True,
             "discord_rpc": True,
             "aggregate_rank_rr": True,
-            "party_finder": Trueremove
+            "party_finder": True
         }
     }
